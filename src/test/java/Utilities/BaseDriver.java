d9e4cc68802b5b66824c65a454b551281eb9ccc9
Elvira <elvira.nzr@gmail.com>
2023-12-13 17:01:14 -0500

    Initial commit

---
+++ src/test/java/Utilities/BaseDriver.java
@@ -0,0 +1,44 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseDriver {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+//            closePreviousDrivers();
+            Logger logger = Logger.getLogger("");
+            logger.setLevel(Level.SEVERE);
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        }
+
+
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+//    public static void closePreviousDrivers() {
+//        try {
+//            Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+}
